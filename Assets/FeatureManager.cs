611d8ea4356037ad9c5ab5f26b1f7de3bebd93f3
aleahblain <59669691+aleahblain@users.noreply.github.com>
2020-09-17 18:07:15 -0400

    Keigo Bday Game

---
+++ Assets/FeatureManager.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class FeatureManager : MonoBehaviour
+{
+    public List<Feature> features;
+    public int currFeature;
+
+    void OnEnable()
+    {
+        LoadFeatures();
+    }
+    void OnDisable()
+    {
+        SaveFeatures();
+    }
+    void LoadFeatures()
+    {
+        features = new List<Feature>();
+        features.Add(new Feature("Hairs", transform.Find("Face").Find("Hair").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Eyes", transform.Find("Face").Find("Eyes").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Mouths", transform.Find("Face").Find("Mouth").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Blushes", transform.Find("Face").Find("Blushes").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Shirts", transform.Find("Shirts").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Pants", transform.Find("Pants").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Jackets", transform.Find("Jackets").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Shoes", transform.Find("Shoes").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Necklaces", transform.Find("Necklaces").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Earrings", transform.Find("Earrings").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Bracelets", transform.Find("Bracelets").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("FaceStuff", transform.Find("FaceStuff").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Items", transform.Find("Items").GetComponent<SpriteRenderer>()));
+
+        for (int i = 0; i < features.Count; i++)
+        {
+            string key = "FEATURE_" + i;
+            if (PlayerPrefs.HasKey(key))
+                PlayerPrefs.SetInt(key, features[i].currIndex);
+            features[i].currIndex = PlayerPrefs.GetInt(key);
+            features[i].UpdateFeatures();
+        }
+
+    }
+    void SaveFeatures()
+    {
+        for (int i = 0; i < features.Count; i++)
+        {
+            string key = "FEATURE_" + i;
+            PlayerPrefs.SetInt(key, features[i].currIndex);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void SetCurrent(int index)
+    {
+        if(features == null)
+        {
+            return;
+        }
+        currFeature = index;
+    }
+
+    public void NextChoice()
+    {
+        if(features == null)
+        {
+            return;
+        }
+
+
+        features[currFeature].currIndex++;
+        features[currFeature].UpdateFeatures();
+}
+
+    public void PreviousChoice()
+    {
+        if (features == null)
+        {
+            return;
+        }
+
+        features[currFeature].currIndex--;
+        features[currFeature].UpdateFeatures();
+    }
+}
+
+
+[System.Serializable]
+public class Feature
+{
+    public string ID;
+    public int currIndex;
+    public Sprite[] choices;
+    public SpriteRenderer renderer;
+    public Feature(string id, SpriteRenderer rend)
+    {
+        ID = id;
+        renderer = rend;
+        UpdateFeatures();
+    }
+    public void UpdateFeatures()
+    {
+        choices = Resources.LoadAll<Sprite>("K5 Textures/" + ID);
+
+        if (choices == null || renderer == null)
+        {
+            return;
+        }
+        if (currIndex < 0)
+        {
+            currIndex = choices.Length - 1;
+        }
+        if (currIndex >= choices.Length)
+        {
+            currIndex = 0;
+        }
+
+        renderer.sprite = choices[currIndex];
+
+    }
+
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform btnPrefab;
+    private FeatureManager mgr;
+    private Text descText;
+    private List<Button> buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mgr = FindObjectOfType<FeatureManager>();
+        descText = transform.Find("Navigation").Find("Text").GetComponent<Text>();
+        transform.Find("Navigation").Find("Previous").GetComponent<Button>().onClick.AddListener(() => mgr.PreviousChoice());
+        transform.Find("Navigation").Find("Next").GetComponent<Button>().onClick.AddListener(() => mgr.NextChoice());
+        InitializeFeatureButtons();
+    }
+
+    void InitializeFeatureButtons()
+    {
+        buttons = new List<Button>();
+
+        float height = btnPrefab.rect.height;
+        float width = btnPrefab.rect.width;
+
+        int numButtons = 0;
+
+        for(int i = 0; i < mgr.features.Count; i++)
+        {
+            RectTransform temp = Instantiate<RectTransform>(btnPrefab);
+            temp.name = i.ToString();
+            temp.SetParent(transform.Find("Features").GetComponent<RectTransform>());
+            temp.localScale = new UnityEngine.Vector3(1, 1, 1);
+            temp.localPosition = new UnityEngine.Vector3(0, 0, 0);
+
+            if(i >= 7)
+            {
+                temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, width, width);
+                temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, numButtons * height, height);
+                numButtons++;
+            } else
+            {
+                temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, width);
+                temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, i * height, height);
+            }  
+
+            Button b = temp.GetComponent<Button>();
+            b.onClick.AddListener(() => mgr.SetCurrent(int.Parse(temp.name)));
+            buttons.Add(b);
+
+        }
+
+    }
+    void UpdateFeatureButtons()
+    {
+        for(int i = 0; i < mgr.features.Count; i++)
+        {
+            buttons[i].transform.Find("FeatureImage").GetComponent<Image>().sprite = mgr.features[i].renderer.sprite;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFeatureButtons();
+        if (mgr.features[mgr.currFeature].ID == "FaceStuff")
+        {
+            descText.text = "Headgear #" + (mgr.features[mgr.currFeature].currIndex + 1).ToString();
+        }
+        else
+        {
+
+            descText.text = mgr.features[mgr.currFeature].ID + " #" + (mgr.features[mgr.currFeature].currIndex + 1).ToString();
+
+        }
+    }
+}
